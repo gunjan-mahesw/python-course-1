bf73c50d237b8baa17ffa048bbde96d14a078a32
Gunjan Maheswari <gunjanmaheswari002@gmail.com>
2025-08-27 06:18:01 +0000

    Day 7:operators in python

--- main.py
+++ main.py
@@ -1,21 +1,7 @@
-a=1
-b=True
-c="Harry"
-d=None
-print(a)
-print(b)
-a1=9
-print(a+a1)
-print("The type of a is",type(a))
-print("the type of b is ",type(b))
-print("the type of c is ",type(c))
-List1=[8,2.3,[4,5],["apple","banana"]]
-print(List1)
-tuple1=(("parrot","sparrow"),("lion","tiger"))
-print(tuple1)
-dict1={"name":"Sakshi",age:20,"canVote":"true"}
-print(dict1)
-#mutable:can be changed 
-#immutable:can't be changed 
-#Dictionary;collection of key-value pairs
-#we know that the pyhton is a object 
+print(5+6)
+print(15-6)
+print(15*6)
+print(15/6)
+print(15//6)
+print(5%3)
+print(2**4)
