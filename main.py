ca5c9973a909cff1d6ca8d783605920287f1c163
Gunjan Maheswari <gunjanmaheswari002@gmail.com>
2025-08-27 06:11:52 +0000

    day 6:Variables and Data Types

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-'''
-Hey,Harry! please don't remove this line 
-Author:Harry
-Course:100 days of code with python 
-'''
-#print("hello world!")
-#this is a single line comment
-#print("this is a print statement.")
-#print("hello world")
-print ("Hey",6,7,sep="~",end="009\n")
-print("Harry")
+a=1
+b=True
+c="Harry"
+d=None
+print(a)
+print(b)
+a1=9
+print(a+a1)
+print("The type of a is",type(a))
+print("the type of b is ",type(b))
+print("the type of c is ",type(c))
+List1=[8,2.3,[4,5],["apple","banana"]]
+print(List1)
+tuple1=(("parrot","sparrow"),("lion","tiger"))
+print(tuple1)
+dict1={"name":"Sakshi",age:20,"canVote":"true"}
+print(dict1)
+#mutable:can be changed 
+#immutable:can't be changed 
+#Dictionary;collection of key-value pairs
+#we know that the pyhton is a object 
