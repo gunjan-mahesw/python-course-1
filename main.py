58614f18a9f3d3c3c70ad667097d5606eddbfe10
Gunjan Maheswari <gunjanmaheswari002@gmail.com>
2025-08-27 05:16:09 +0000

    day 5 :comments,escape sequences and print statement

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-print("Hello World!,7")
-print(5)
-print("Byee")
-print(17*13)
+'''
+Hey,Harry! please don't remove this line 
+Author:Harry
+Course:100 days of code with python 
+'''
+#print("hello world!")
+#this is a single line comment
+#print("this is a print statement.")
+#print("hello world")
+print ("Hey",6,7,sep="~",end="009\n")
+print("Harry")
